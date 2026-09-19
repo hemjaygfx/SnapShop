2cc19171d4adceb94a2ace3f8fa36215018d41c2
hemjaygfx <hemjaystarz@gmail.com>
2026-09-19 21:49:47 +0100

    chore: remove ProductDetailPage and its route
    delete the ProductDetailPage component file, remove its import from App.jsx and comment out its route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import HomePage from "./pages/HomePage";
 import OrdersPage from "./pages/OrdersPage";
 import CartPage from "./pages/CartPage";
 import CheckoutReturnPage from "./pages/CheckoutReturnPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -21,7 +20,8 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/orders" element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />} />
         <Route path="/checkout/return" element={<CheckoutReturnPage />} />
-        <Route path="/product/:slug" element={<ProductDetailPage />} />
+
+        {/* <Route path="/product/:slug" element={<ProductDetailPage />} /> */}
 
         {/* <Route path="/demo-sentry" element={<SentryDemoPage />} /> */}
         {/* <Route path="/orders/:id/call" element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />} /> */}
